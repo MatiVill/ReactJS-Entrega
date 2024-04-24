3ddb0e96d43ffb70d61de6d68fea92f9a1df4644
Matías Villarroel <cmvillarroel@icloud.com>
2024-04-24 00:28:39 -0400

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar"
+import './App.css'
+
+
+function App() {
+
+
+
+
+  return (
+<div>
+  <Navbar />
+  <h1>Hola mundo!</h1>
+</div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+
+    return(
+        <nav>
+            <h2>Componente Navbar</h2>
+            <ul>
+                <li>Kimonos</li>
+                <li>Nogi</li>
+                <li>Street</li>
+                <li>Accesorios</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
